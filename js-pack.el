e972329c6bd48a7d62ac49ca758d9783f4ae2377
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-10-17 11:50:18 +0200

    Add default modes

---
+++ js-pack.el
@@ -0,0 +1,17 @@
+;;; js-pack.el --- Javascript configuration
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'install-packages-pack)
+(install-packs '(js2-mode
+                 flymake-jslint))
+
+(require js2-mode)
+(require 'flymake-jslint)
+
+(add-hook 'js-mode-hook 'flymake-jslint-load)
+
+(provide 'js-pack)
+;;; js-pack.el ends here
