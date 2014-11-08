9083f3b7ff707e2abeab47e3f73f93f0f02235ca
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-11-08 14:58:41 +0100

    Add repl switch for js2mode buffer

--- js-pack.el
+++ js-pack.el
@@ -6,12 +6,20 @@
 
 (require 'install-packages-pack)
 (install-packages-pack/install-packs '(js2-mode
-                                       flymake-jshint))
+                                       flymake-jshint
+                                       repl-toggle
+                                       nodejs-repl))
 
 (require 'js2-mode)
 (require 'flymake-jshint)
 
-(add-hook 'js2-mode-hook 'flymake-jshint-load)
+(add-hook 'js2-mode-hook 'flymake-jshint-load)j
+
+(require 'nodejs-repl)
+(require 'repl-toggle)
+
+(rtog/add-repl 'js2mode 'nodejs-repl)
+(setq rtog/goto-buffer-fun 'pop-to-buffer)
 
 (provide 'js-pack)
 ;;; js-pack.el ends here
