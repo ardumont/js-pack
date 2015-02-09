3f9b7ca70f08ee3771d27e0bf851a5a0a767b17a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-02-09 20:02:46 +0100

    Revert "Tryout - using js-comint for javascript repl"
    This reverts commit 5416debe2a400b0fa208310ddec7ba174083953e.

--- js-pack.el
+++ js-pack.el
@@ -12,9 +12,8 @@
                                        web-mode
                                        smartscan
                                        js2-refactor
-                                       ;; skewer-mode
-                                       ;; slime-js
-                                       js-comint))
+                                       skewer-mode
+                                       slime-js))
 
 (require 'js2-mode)
 
@@ -44,50 +43,26 @@
 
 (js2r-add-keybindings-with-prefix "C-c C-j")
 
-;;(skewer-setup)
+(skewer-setup)
 
 (require 'repl-toggle)
 
-;; (rtog/add-repl 'js2-mode 'skewer-repl)
-;; (setq rtog/goto-buffer-fun 'pop-to-buffer)
+(rtog/add-repl 'js2-mode 'skewer-repl)
+(setq rtog/goto-buffer-fun 'pop-to-buffer)
 
 ;; swank-js/slime-js setup
 
 ;; pre-requisite: install `'npm install swank-js`'
 
-;; (require 'slime-js)
-;; (add-hook 'js2-mode-hook (lambda () (slime-js-minor-mode 1)))
+(require 'slime-js)
+(add-hook 'js2-mode-hook (lambda () (slime-js-minor-mode 1)))
+
+(add-hook 'css-mode-hook
+          (lambda ()
+            (define-key css-mode-map "\M-\C-x" 'slime-js-refresh-css)
+            (define-key css-mode-map "\C-c\C-r" 'slime-js-embed-css)))
 
 ;; Now go: M-x slime-connect RET localhost RET 4005 RET
-
-;; (add-hook 'css-mode-hook
-;;           (lambda ()
-;;             (define-key css-mode-map "\M-\C-x" 'slime-js-refresh-css)
-;;             (define-key css-mode-map "\C-c\C-r" 'slime-js-embed-css)))
-
-(setq inferior-js-program-command "node")
-
-;;  Do: M-x run-js
-;;  Away you go.
-
-;; borrowed from @purcell - https://github.com/purcell/emacs.d/blob/master/lisp/init-javascript.el
-
-(defvar inferior-js-minor-mode-map (make-sparse-keymap))
-(define-key inferior-js-minor-mode-map "\C-x\C-e" 'js-send-last-sexp)
-(define-key inferior-js-minor-mode-map "\C-\M-x" 'js-send-last-sexp-and-go)
-(define-key inferior-js-minor-mode-map "\C-cb" 'js-send-buffer)
-(define-key inferior-js-minor-mode-map "\C-c\C-b" 'js-send-buffer-and-go)
-(define-key inferior-js-minor-mode-map "\C-cl" 'js-load-file-and-go)
-
-(define-minor-mode inferior-js-keys-mode
-  "Bindings for communicating with an inferior js interpreter."
-  nil " InfJS" inferior-js-minor-mode-map)
-
-(dolist (hook '(js2-mode-hook js-mode-hook))
-  (add-hook hook 'inferior-js-keys-mode))
-
-(rtog/add-repl 'js2-mode 'run-js)
-(setq rtog/goto-buffer-fun 'pop-to-buffer)
 
 (provide 'js-pack)
 ;;; js-pack.el ends here
